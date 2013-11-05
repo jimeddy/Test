386786364c22eb3dbfe1add60df637199161b18d
jimeddy <jim.eddy@mac.com>
2013-11-05 13:25:45 -0500

    Just updated this script

---
+++ test.py
@@ -0,0 +1,36 @@
+# Jim Eddy
+# CS - 21
+# Exercise 4-3 (Mass and Weight)
+
+# Global Constant
+GRAVITY = 9.8
+
+def main():
+
+    mass = float(input('Enter a mass in kilograms: '))
+
+    if mass > 0:
+        show_weight(mass)
+    else:
+        print('Please enter a positive, non-zero value for mass.')
+
+def show_weight(mass):
+  
+    # calculate weight
+    weight = mass * GRAVITY
+    
+    # test for > 1000 newtons
+    if weight > 1000:
+        print('The object weighs',format(weight, '.2f'),
+              'newtons, and is too heavy!') 
+
+    elif weight < 10:
+        print('The object weighs',format(weight, '.2f'),
+              'newtons, and is too light!')
+
+    else:
+        print('The object weighs',format(weight, '.2f'),
+              'newtons, and is within the acceptable range.')
+
+main()
+
